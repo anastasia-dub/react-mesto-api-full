f097143151cbde7d4d53831d4ccc026e1d8e7561
Anastasia Dubkovetskaya <k1tanas@yandex.ru>
2022-01-29 14:20:07 +0200

    Изменения

--- backend/app.js
+++ backend/app.js
@@ -37,8 +37,8 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(URL_REGEXP),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
